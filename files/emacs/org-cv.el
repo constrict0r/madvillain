48690844d5bc73f0ba99201838361b66f536786b
constrict0r <constrict0r@protonmail.com>
2020-02-06 02:07:49 -0600

    Adds disabled org-cv task and related files

---
+++ files/emacs/org-cv.el
@@ -0,0 +1,16 @@
+;; Install required packages if not installed.
+(mapc #'(lambda (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+      '(use-package)
+)
+
+(require 'use-package)
+
+(use-package ox-moderncv
+    :load-path "~/org/org-cv/"
+    :init (require 'ox-moderncv))
+
+ (use-package ox-moderncv
+     :load-path "~/org/org-cv/"
+     :init (require 'ox-altacv))
